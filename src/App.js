66a529425f55e8344e31598a803e93d93cc80924
Lucas Cezar <lmcezar@gmail.com>
2019-04-08 14:50:07 -0300

    Listagem dos produtos com estilização

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
+import React, { Component } from "react";
+import Header from "./components/Header";
 import Main from "./pages/main";
 
 import "./styles.css";
 
 const App = () => (
-      <div className="App">
-        <Header />
-        <Main />
-      </div>
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
 );
 
 export default App;
