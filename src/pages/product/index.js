1503b3cdba0daf874ff26f6b061b99090849aa1b
Lucas Cezar <lmcezar@gmail.com>
2019-04-15 13:53:14 -0300

    Configuração da navegacao

---
+++ src/pages/product/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+// import { Container } from './styles';
+
+export default class Product extends Component {
+  render() {
+    return <h1>Product</h1>;
+  }
+}
